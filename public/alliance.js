d91a8283d81d76e99db1f681a91c82e07f6c0ea4
Michael Sergio <mikeserg@gmail.com>
2012-07-20 02:35:27 -0400

    color svg map with backbone integration

--- public/alliance.js
+++ public/alliance.js
@@ -1,317 +1,410 @@
-google.load('visualization', '1.0', {'packages':['geochart']});
-google.setOnLoadCallback(ready);
-
-function ready() {
-  showChart();
-  
-}
-
-
-var stupidCountryIds = {
-  //soviet union
-  "su": "path9794",
-  // west germany
-  "wd": "path8375",
-  // east germany
-  "od": "path8364",
-  // india without burma (myanmar)
-  "in": "path9828",
-  // yugoslavia - collapsed 91-03
-  "yu": "path8403",
-  // saudia arabia
-  "sa": "path3380",
-  // iran - destroyed by revolution
-  "ir": "path4606",
-  // indonesia
-  "ri": "path2206",
-  // west papau - not yet part of indonesia
-  "wp": "path2562",
-  // Sabah & Sarawak - present east malaysia
-  "sw": "path2560",
-  // west malaysia
-  "my": "path4722",
-};
-
-function showChart() {
-  var type = getType();
-  drawChart(type, year);
-}
-
-function drawChart(type) {
-  var url;
-  var BASE = "https://spreadsheets.google.com/spreadsheet/pub";
-  switch (type){
-    case "military":
-      url = BASE + "?key=0Asm_G8nr4TCSdG1nNjk5RzItcUp6N2dSdHUwOENXa0E&gid=0";
-      type_label = "Army Population";
-      break;
-    case "civilian":
-      url = BASE + "?key=phAwcNAVuyj0XOoBL_n5tAQ";
-      type_label = "Civilian Population";
-      break;
-    case "agriculture":
-      url = BASE + "?key=0AkBd6lyS3EmpdFhPbDdCTTYxM1dGc21UdE9sSkp1WEE";
-      type_label = "Agriculture Output";
-      break;
-    case "industry":
-      url = BASE + "?key=0AkBd6lyS3EmpdHA2UEFOYTlUTWtzV29xbHFuMU00SFE";
-      type_label = "Industrial Output";
-    break;
-    case "alliances":
-      url = BASE + "?key=0AhRtQr8CRozEdEMwaHBwWDl4NUhSTUxBZ2FOa25ZLWc";
-      type_label = "Alliances";
-      break;
-  }
-  //test();
-
-  if (type === 'alliances'){
-    queryAlliances(url);
-  }
-}
-function queryAlliances(url) {
-
-  var query_string = 'select A,B,C,D where B = ' + getYear();
-
-  var query = new google.visualization.Query(url);
-  query.setQuery(query_string);
-  query.send(showAlliances);  
-  console.log(url);
-  console.log(query);
-}
-
-function showAlliances(response) {
-  if (response.isError()) {
-    alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
-    return;
-  }
-
-  var data = response.getDataTable();
-  var i;
-
-  console.log(data);
-
-  for (i = 0; i < data.D.length; i++) {
-    applyAlliance(data.D[i].c);
-  }
-  
-  console.log(data.D.length);
-
-
-
-  var year_string = getYear() + ' A.D.';
-  document.getElementById("visualization_year").innerHTML=year_string;
-
-}
-
-function setDisplayInfo(cc) {
-  countryApply(cc, function(country) {
-    country.onclick = function() {
-      document.getElementById("info").innerHTML=cc;
-    };
-  });
-
-}
-
-function applyAlliance(alliance) {
-  var name, color, members;
-  name = alliance[0].v;
-  color = alliance[2].v;
-  members = alliance[3].v.split(/\s+/);
-
-  console.log(members);
-  makeAlliance.apply(color, members);
-
-
-  var colorDiv = document.createElement('p');
-  var nameDiv = document.createElement('p');
-
-  var newContent = document.createTextNode(" " + name + " | ");
-  var colorContent = document.createTextNode("■");
-
-  colorDiv.style.color = color;
-  colorDiv.style.display = 'inline';
-
-  nameDiv.style.display = 'inline';
-
-  nameDiv.appendChild(newContent);
-  colorDiv.appendChild(colorContent);
-
-  document.getElementById('alliances').appendChild(colorDiv);
-  document.getElementById('alliances').appendChild(nameDiv);
-
-}
-
-function makeAlliance() {
-  var i;
-  for (i=0; i < arguments.length; i++) {
-    cc = arguments[i];
-    setMouseOver(cc);
-    colorCountry(cc, this);
-    console.log(cc);
-    //setDisplayInfo(cc);
-  }
-}
-
-function get_year_col(type, year) {
-  var delta_year, start_year_col;
-
-  switch (type){
-    case "military":
-      delta_year = 1985;
-      start_year_col = 'B';
-      break;
-    case "civilian":
-      delta_year = 1785;
-      start_year_col = 'E';
-      break;
-    case "agriculture":
-      delta_year = 1960;
-      start_year_col = 'B';
-      break;
-    case "industry":
-      delta_year = 1960;
-      start_year_col = 'B';
-      break;
-    default:
-      return '';
-  }
-
-  var delta = year - delta_year;
-  var year_col = col_add(start_year_col, delta);
-  return year_col;
-}
-
-function get_type_color() {
-  switch (getType()){
-    case "military":
-      return 'red';
-    case "civilian":
-        return 'blue';
-    case "industry":
-        return 'orange';
-    case "agriculture":
-        return 'green';
-    default:
-      return "purple";
-  }
-}
-function showValue(newValue)
-{
-  document.getElementById("year_val").innerHTML=newValue;
-}
-
-function getType()
-{
-  return document.getElementById("type").value;
-}
-
-function getYear()
-{
-  return document.getElementById("year").value;
-}
-
-function setCountryStroke(cc, stroke, color) {
-  countryApply(cc, function(country) {
-    country.style.stroke = color;
-    country.style.strokeWidth = stroke;
-  });
-}
-
-function opacify(cc, val) {
-  countryApply(cc, function(country) {
-    country.style['fill-opacity'] = val; 
-  });
-}
-
-function setMouseOver(cc) {
-  countryApply(cc, function(country) {
-    //var in_arg = "setCountryStroke('" + country.id + "',  3, 'black')";
-    country.onmouseover = function() {
-      document.getElementById("info").innerHTML=cc;
-      opacify(cc, 0.4);
-      /*
-      $(this.parent()).attr('title', 'aus').tooltip({
-        track: true, 
-        delay: 0, 
-        showURL: false, 
-        showBody: " - ", 
-        fade: 250,
-      });
-      */
-    };
-
-    country.onmouseout = function() {
-      opacify(cc, 1);
-    };
-  });
-}
-
-function getCountry(cc) {
-  return document.getElementById('visualization').contentDocument
-                 .getElementById(cc);
-}
-
-function colorCountry(cc, color) {
-  countryApply(cc, function(country) {
-    country.style.fill = color;
-  });
-}
-
-/*
- * Because countries are made up of mutiple paths,
- * this is used to apply functions to countries.
- * cc -  ISO_3166 country codes + some missing old countries
- *       as found in stupidCountryIds
- * fn - functions(country) country will be a country element
+/**
+ * If your reading this I apologize for the way the code currently looks.
+ * I plan on cleaning this up and open sourcing this soon enough.
+ * However, since you're intrested, I have left this app unminifed 
+ * in its current state for you.
+ *
+ * If you find any bugs before this goes up on git
+ * send them to mikeserg at gmail
+ *
+ * Until then,
+ * Abandon All Hope...
  */
-function countryApply(cc, fn){
-  // using ISO_3166 country codes + some missing old countries
-  // http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
-  var i, paths;
-  cc = stupidCountryIds[cc] || cc;
-  var country = document.getElementById('visualization').contentDocument
-                        .getElementById(cc);
-  fn(country);
+google.load('visualization', '1.0', {'packages':['geochart']});
 
-  paths = country.getElementsByTagName('path');
-  for (i = 0; i < paths.length; i++) {
-    fn(paths[i]);
-  }
-}
+var BASE = "https://spreadsheets.google.com/spreadsheet/pub";
+var SPREADSHEET_QS = "?key=0AhRtQr8CRozEdDhKZ0ZaMTNTaE02NUVDekphSlBJTVE&gid=1";
+var SPREADSHEET_URL = BASE + SPREADSHEET_QS;
 
-function test() {
-  nato();
-  others();
-  warsaw();
-  middleeast();
-  southasia();
-  southpacific();
-
-  $('#visualization_year').tooltip({
-      track: true, 
-      delay: 0, 
-      showURL: false, 
-      showBody: " - ", 
-      fade: 250
+$(function(){
+  var Nation = Backbone.Model.extend({
+    totalPopulation: function() {
+      return this.get('milpop') + this.get('civpop');
+    },
+    totalProduction: function() {
+      return this.get('indupro') + this.get('agripro');
+    }
   });
+
+  var World = Backbone.Collection.extend({
+    model: Nation,
+
+    fetch: function(options) {
+      var collection = this;
+      var query_string = "select *";
+      var query = new google.visualization.Query(SPREADSHEET_URL);
+      query.setQuery(query_string);
+      query.send(function(response) {
+        if (response.isError()) {
+          alert('Error in query: ' + response.getMessage() + ' '
+                 + response.getDetailedMessage());
+          return;
+        }
+
+        var data = response.getDataTable();
+        console.log(data);
+
+        var i;
+        var results = [];
+        // TODO fix name and year 
+        for (i = 0; i < data.D.length; i++) {
+          var nameYear = data.getValue(i,2).split(":");
+          var nation = {
+            id: data.getValue(i, 1),
+            name: nameYear[0],
+            year: nameYear[1].trim(),
+            player: data.getValue(i, 3),
+            civpop: data.getValue(i, 4),
+            milpop: data.getValue(i, 5),
+            nukes: data.getValue(i, 6),
+            agripro: data.getValue(i, 7),
+            indupro: data.getValue(i, 8),
+            alliance: data.getValue(i, 11),
+            territory: data.getValue(i, 12),
+            capital: data.getValue(i, 13)
+          };
+
+          results.push(nation);
+        }
+        collection.reset(results);
+
+        if (options.success) {
+          options.success();
+        }
+      }); 
+
+    },
+
+    getAlliances: function() {
+      var collection = this;
+      var alliances = collection.groupBy(function(nation){
+        var alliance = nation.get('alliance');
+        if (alliance) { 
+          return alliance; 
+        }
+      });
+      delete alliances[undefined];
+      return alliances;
+    },
+
+  });
+
+
+  var ControlView = Backbone.View.extend({
+    el: $('#control'),
+    
+    initialize: function() {
+      this.$typeElreturn = $("#type");
+    },
+
+
+
+    getTypeColor: function(type) {
+      switch (type){
+        case "military":
+          return 'red';
+        case "civilian":
+            return 'blue';
+        case "industry":
+            return 'orange';
+        case "agriculture":
+            return 'green';
+        default:
+          return "purple";
+      }
+    }
+  });
+
 
   /*
-  var year_col = get_year_col(type, year);
 
-  query_string = "select A, "
-                 + year_col
-                 + " label "
-                 + year_col 
-                 + " '"
-                 + type_label
-                 + "'";
-  console.log(query_string);
+  function get_year_col(type, year) {
+    var delta_year, start_year_col;
 
-  var query = new google.visualization.Query(url);
-  query.setQuery(query_string);
-  query.send(handleQueryResponse);  
+    switch (type){
+      case "military":
+        delta_year = 1985;
+        start_year_col = 'B';
+        break;
+      case "civilian":
+        delta_year = 1785;
+        start_year_col = 'E';
+        break;
+      case "agriculture":
+        delta_year = 1960;
+        start_year_col = 'B';
+        break;
+      case "industry":
+        delta_year = 1960;
+        start_year_col = 'B';
+        break;
+      default:
+        return '';
+    }
 
-  console.log(query);
+    var delta = year - delta_year;
+    var year_col = col_add(start_year_col, delta);
+    return year_col;
+  }
+
   */
-}
 
+  function showValue(newValue)
+  {
+    $('#year_val').val(newValue);
+  }
+
+  function getYear()
+  {
+    return $("#year").val();
+  }
+
+  // SVG Map
+  // ============================================
+
+  function opacify(cc, val) {
+    countryApply(cc, function(country) {
+      country.style['fill-opacity'] = val; 
+    });
+  }
+
+  function getCountry(cc) {
+    return document.getElementById('visualization').contentDocument
+                   .getElementById(cc);
+  }
+
+  function colorCountry(cc, color) {
+    countryApply(cc, function(country) {
+      if (country === null) {
+        console.log("Country not found: " + cc);
+      }
+      else {
+        country.style.fill = color;
+      }
+    });
+  }
+
+  /*
+   * Because countries are made up of mutiple paths,
+   * this is used to apply functions to countries.
+   * cc -  ISO_3166 country codes + some missing old countries
+   *       as found in stupidCountryIds
+   * fn - functions(country) country will be a country element
+   */
+  function countryApply(cc, fn){
+    // using ISO_3166 country codes + some missing old countries
+    // http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
+    var i, paths;
+    //cc = stupidCountryIds[cc] || cc;
+    var country = document.getElementById('visualization').contentDocument
+                          .getElementById(cc);
+    if (country) {
+      fn(country);
+
+      paths = country.getElementsByTagName('path');
+      for (i = 0; i < paths.length; i++) {
+        fn(paths[i]);
+      }
+    }
+    else {
+      console.log("Country not found: " + cc);
+    }
+  }
+
+  var InfoView = Backbone.View.extend({
+    el: $('#info'),
+
+    showNation: function(cc) {
+      var nation = this.collection.get(cc);
+      this.$el.find('#name').text(nation.get('name'));
+      this.$el.find('#year').text(nation.get('year'));
+      this.$el.find('#civpop').text(nation.get('civpop'));
+      this.$el.find('#milpop').text(nation.get('milpop'));
+      this.$el.find('#nukes').text(nation.get('nukes'));
+      this.$el.find('#agripro').text(nation.get('agripro'));
+      this.$el.find('#indupro').text(nation.get('indupro'));
+      this.$el.find('#alliance').text(nation.get('alliance'));
+      this.$el.find('#territory').text(nation.get('territory'));
+      this.$el.find('#capital').text(nation.get('capital'));
+      this.$el.find('#totalpop').text(nation.totalPopulation());
+      this.$el.find('#totalpro').text(nation.totalProduction());
+
+      this.$el.find('#flag').attr('class', 'flag ' + nation.get('id'));
+    }
+  });
+
+  var SearchView = Backbone.View.extend({
+    el: $('#search'),
+
+    initialize: function() {
+      this.infoview = this.options.infoview;
+      
+      // all names
+      var nations = this.collection.map(function(nation) {
+        return nation.get('name');
+      });
+
+      // build reverse map
+      this.reverseMap = this.collection.reduce(function(map, nation) {
+        map[nation.get('name')] = nation.get('id');
+        return map;
+      }, {}, this);
+
+  
+      var searchview = this;
+      var ac = this.$el.autocomplete({
+        source: nations,
+        select: function(event, ui) {
+          var selected = ui.item;
+
+          ac.autocomplete('close');
+          //this.text(selected);
+          var cc = searchview.reverseMap[selected.value];
+          var nation = searchview.collection.get(cc);
+          searchview.infoview.showNation(nation);
+        }  
+      });
+
+    },
+  });
+
+
+
+  var MapView = Backbone.View.extend({  
+    el: $('#visualization'),
+
+    initialize: function() {
+      this.colorI = -1;
+      this.infoview = this.options.infoview;
+      this.activateAll();
+
+    },
+
+    activateAll: function() {
+      var nations = _.map(this.collection.models, function(member) {
+        return member.id;
+      });
+      _.each(nations, function(cc) {
+        colorCountry(cc, this.randomColor());
+        this.setMouseOver(cc);
+      }, this);
+      
+      colorCountry('ocean', '#D6FFFF');
+      colorCountry('aq', '#E6FFFF');
+    },
+
+    colorAllKnownAlliances: function() {
+      //TODO need to reset colors
+      var map = this;
+      var allies = this.collection.getAlliances();
+      _.each(allies, function(val, key) {
+        map.applyAlliance(key, val);
+      });
+    },
+
+    getAllianceColor: function(name) {
+      switch(name) {
+        case "NATO": return 'blue';
+        case "USSR": return 'brown';
+      }
+
+      //this.colorI = this.colorI === undefined ? 0 : this.colorI + 1;
+      this.colorI++;
+      var colorList = ['green', 'orange', 'yellow', 'purple', 'cyan', 'magenta',
+                       'lightgreen', 'red'];
+      if (this.colorI > colorList.length) {
+        return 'grey';
+      }
+
+      return colorList[this.colorI];
+    },
+
+    applyAlliance: function(name, members) {
+      var color = this.getAllianceColor(name);
+
+      // set the map element colors
+      members = _.map(members, function(member) {
+        return member.id;
+      });
+      this.makeAlliance(color, members);
+
+      // make legend
+      var colorDiv = $('<p>');
+      colorDiv.text("■");
+      colorDiv.css('color', color);
+      colorDiv.css('display', 'inline');
+
+
+      var nameDiv = $('<p>');
+      nameDiv.text(" " + name + " | ");
+      nameDiv.css('display', 'inline');
+
+      $('#alliances').append(colorDiv).append(nameDiv);
+    },
+
+    makeAlliance: function(color, members) {
+      _.each(members, function(cc) {
+        this.setMouseOver(cc);
+        colorCountry(cc, color);
+      }, this);
+    },
+
+    //TODO this will handle locking
+    setDisplayInfo: function(cc) {
+      var map = this;
+      countryApply(cc, function(country) {
+        country.onclick = function() {
+          map.infoview.showNation(cc);
+        };
+      });
+    },
+
+    setMouseOver: function(cc) {
+      var map = this;
+      countryApply(cc, function(country) {
+        country.onmouseover = function() {
+          map.infoview.showNation(cc);
+          opacify(cc, 0.4);
+          //XXX add tooltip?
+        };
+
+        country.onmouseout = function() {
+          opacify(cc, 1);
+        };
+      });
+    },
+
+    randomColor: function() {
+      var rc = function() { return Math.floor(Math.random() * 256); };
+      return 'rgb(' + rc() + ',' + rc() + ',' + rc() + ')';
+    },
+
+    colorResources: function() {
+
+    }
+
+
+  });
+
+
+  //TODO useless?
+  function ready() {
+    showChart();
+  }
+
+  function pullAllData() {
+    var world = new World();
+    world.fetch({
+      success: function(){
+        console.log(world);
+
+        var infoview = new InfoView({collection: world});
+        var searchview = new SearchView({collection: world, infoview:infoview});
+        var map = new MapView({collection: world, infoview: infoview});
+      }
+    });
+  }
+
+  //pullAllData();
+  google.setOnLoadCallback(pullAllData);
+});
